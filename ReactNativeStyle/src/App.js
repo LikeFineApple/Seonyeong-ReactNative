a786041f6d6907beef597394f27e97fabb03b3fc
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-08-20 16:50:53 +0900

    feat : styled component input

--- ReactNativeStyle/src/App.js
+++ ReactNativeStyle/src/App.js
@@ -6,7 +6,7 @@ import React from 'react';
 // import ShadowBox from './components/ShadowBox';
 import styled from 'styled-components/native';
 import Button from './components/Button';
-
+import Input from './components/Input';
 
 const Container = styled.View`
     flex:1;
@@ -21,6 +21,7 @@ const App =() => {
         <Container>
             <Button title="Hanbit" />
             <Button title="React Native" />
+            <Input />
         </Container>
     );
 };
